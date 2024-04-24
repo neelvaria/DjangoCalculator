d50ddaca543426bbf835b9e0bd17b4ee431939d6
neelvaria <neelvaria08@gmail.com>
2024-04-24 15:46:51 +0530

    calculatordone

--- calapp/views.py
+++ calapp/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
-# Create your views here.
+def indexpage(request):
+    return render(request, "index.html")
+
+def submitquery(request):
+    q = request.GET.get('query', None)
+    if q is not None:
+        try:
+            ans = eval(q)
+            mydict = {
+                "q": q,
+                "ans": ans,
+                "error": False,
+                "result": True
+            }
+        except Exception as e:
+            mydict = {
+                "error": True,
+                "result": False
+            }
+    else:
+        mydict = {
+            "error": True,
+            "result": False
+        }
+
+    return render(request, "index.html", context=mydict)
